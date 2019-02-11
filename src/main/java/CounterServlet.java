822bc18069733087021480a8fa76312b99a50de3
Sam Rodriguez <sam27rodriguez@gmail.com>
2019-02-10 20:20:06 -0600

    servlets exercise update

--- src/main/java/CounterServlet.java
+++ src/main/java/CounterServlet.java
@@ -1,16 +1,16 @@
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/home")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet(name = "CounterServlet", urlPatterns = "/count")
+public class CounterServlet extends HttpServlet {
     private int counter = 0;
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.getWriter().println("<h1>Hello, World!</h1>");
-        response.getWriter().println("You have refreshed the page " + counter + " times");
-        counter += 5;
+        counter += 10;
+        response.getWriter().println("<h1>The count is " + counter + ".</h1>");
+
     }
 }
